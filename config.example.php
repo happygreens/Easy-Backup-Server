3d5dfc8a52bd1e69d96d0c1316cd5a25f99e782e
marioberg.de <hyperword@gmail.com>
2026-08-03 11:57:02 +0200

    Delete config.example.php

--- config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Dies ist eine Beispiel-Datei für den Nutzer-Server
-// Administration und V-Server Konfiguration
-
-define('SERVER_PANEL', 'KeyHelp');
-define('PROMO_CODE', '342BZCR2U'); 
-
-echo "Willkommen im Administrationsbereich. Panel: " . SERVER_PANEL;
-?>
